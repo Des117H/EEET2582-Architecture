5d0b1e0a5722bb73d8637f92a74ef664b5536379
quynguyn <s3878636@rmit.edu.vn>
2023-12-19 19:34:37 +0700

    new code

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import {
+    initializeApp
+
+} from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ52ZecgR_HY-AJ5oSisUdm2SPhoYEZ40",
+    authDomain: "architecture-grandma-bea3b.firebaseapp.com",
+    databaseURL: "https://architecture-grandma-bea3b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "architecture-grandma-bea3b",
+    storageBucket: "architecture-grandma-bea3b.appspot.com",
+    messagingSenderId: "390073017040",
+    appId: "1:390073017040:web:0ce22fda1c9c80d2c3f4d5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
