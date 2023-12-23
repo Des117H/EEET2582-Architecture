c49684ec4e2e1a2718e4e655f536a8d49edbb90e
quynguyn <s3878636@rmit.edu.vn>
2023-12-23 10:29:00 +0700

    apply scss and delete login,js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 "use client";
 
-import { Button, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import AppHeader from "../components/app.header";
 import AppFooter from "../components/app.footer";
-import { useRouter } from "next/navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Stack, Modal,Image } from 'react-bootstrap';
+
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { Button, CircularProgress, Container, Dialog, Typography } from '@mui/material';
+import { useAuth } from '../firebase/auth';
+import { auth } from '../firebase/firebase';
+import styles from '../styles/index.module.scss';
+
+
+// Configure FirebaseUI
+const REDIRECT_PAGE = "/dashboard";
+
+const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: REDIRECT_PAGE,
+  // able to log In with email and google
+  SignInOptions: [
+    EmailAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
 
 const Welcome = () => {
   const router = useRouter();
+  const { authUser, isLoading } = useAuth();
+  const [login, setLogin] = useState(false);
 
   return (
     <div>
@@ -15,14 +40,18 @@ const Welcome = () => {
         <AppHeader />
       </header>
       <main>
-        <Container className="description">
-          <div className="text-box">
-            <h2>
-              Text with love,
-              <br />
-              like your <span>Grandma</span>
-            </h2>
-            <p>
+        <Stack direction='horizontal' gap={10}>
+          <div className='p-2'></div>
+        <Container  className="description p-2">
+          <div className="text-box" >
+          
+              <Typography variant='h2' >
+                Text with love,
+                <br />
+                like your <span>Grandma</span>
+              </Typography>
+           
+            <h5>
               Grandmaly is a Large Language Model AI system with the purpose of
               fixing languid errors of an English manuscript, such errors
               including grammatical mistakes, typos, word use, and punctuation.
@@ -33,17 +62,30 @@ const Welcome = () => {
               With an account, you will be provided with the option to subscribe
               and enjoy extended perks of Grandmaly. You will also be given a
               2-week trial period before deciding to subscribe.
-            </p>
+            </h5>
             <Button
-              variant="primary"
-              onClick={() => {
-                router.push("/login");
-              }}
-            >
-              Sign Up
-            </Button>
+            variant="contained"
+            color="primary"
+            style={
+              {float: "right", }
+            }
+            onClick={() => setLogin(true)}
+          >
+            Login / Regist
+          </Button>
+          <Dialog  className={styles.mydialog} onClose={() => setLogin(false)} open={login}>
+            <Typography variant="h4" className={styles.title}> Log In</Typography>
+            <hr/>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+          </Dialog>
           </div>
         </Container>
+        <Container className='p-2'>
+              <Image src="Image1.png" alt='Image of grandma' fluid>
+        
+              </Image>
+        </Container>
+        </Stack>
       </main>
       <footer>
         <AppFooter />
