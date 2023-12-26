948932c58daf9088d6651071bc332d3ec342fa35
quynguyn <s3878636@rmit.edu.vn>
2023-12-26 17:36:44 +0700

    add momoth

--- pages/editDoc.js
+++ pages/editDoc.js
@@ -1,10 +1,7 @@
 "use client";
 
-import AppHeader from "../components/app.header";
 import AppFooter from "../components/app.footer";
 import { useRouter } from "next/router";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import { EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import {
 	Button,
 	CircularProgress,
@@ -14,17 +11,19 @@ import {
 } from "@mui/material";
 import styles from "../styles/global.module.css";
 import Head from "next/head";
-import documentEdit from "../components/editQuill";
 
 
 import dynamic from "next/dynamic";
 import React, { useState, useRef, useEffect } from "react";
 import 'react-quill/dist/quill.snow.css'
+import {getDownloadURL} from '../firebase/storage';
+
 
 const ReactQuill = dynamic(() => import("react-quill"), {
   ssr: false
 });
 
+console.log(getDownloadURL());
 
 const EditDocument = () => {
   const router = useRouter();
@@ -37,12 +36,14 @@ const EditDocument = () => {
   }, [quillRef]);
 
   return (
+   
     <div>
       <Head>
         <title>Document Edit</title>
       </Head>
 
       <main className={styles.mainBody} style={{ padding: '20px' }} >
+        
       <ReactQuill ref={quillRef} theme="snow" value={value} onChange={setValue} />
       </main>
       <AppFooter/>
