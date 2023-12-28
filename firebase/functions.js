8a2e9eee1bf8d83948d7f2a83692809ea74a7cb2
Des117H <50516919+Des117H@users.noreply.github.com>
2023-12-28 09:15:23 +0700

    add license key of html to doc

--- firebase/functions.js
+++ firebase/functions.js
@@ -1,4 +1,9 @@
 import { functions } from "./firebase";
 import { httpsCallable } from "firebase/functions";
+// import HTMLtoDOCX from '../dist/html-to-docx.esm';
 
 export const processDocument = httpsCallable(functions, 'processDocument');
+
+export const html2Document = () => {
+
+}
--- pages/editDoc.js
+++ pages/editDoc.js
@@ -5,11 +5,11 @@ import { Stack, Modal, Image } from "react-bootstrap";
 
 import { useRouter } from "next/router";
 import {
-  Button,
-  CircularProgress,
-  Container,
-  Dialog,
-  Typography,
+	Button,
+	CircularProgress,
+	Container,
+	Dialog,
+	Typography,
 } from "@mui/material";
 import styles from "../styles/global.module.css";
 import Head from "next/head";
@@ -20,84 +20,121 @@ import "react-quill/dist/quill.snow.css";
 import { getDownloadURL } from "../firebase/storage";
 import { auth } from "../firebase/firebase";
 import mammoth from "mammoth";
-// import HTMLtoDOCX from 'html-to-docx';
+import WebViewer from '@pdftron/webviewer'
 
 const ReactQuill = dynamic(() => import("react-quill"), {
-  ssr: false,
+	ssr: false,
 });
 
-const EditDocument = () => {
-  const router = useRouter();
+export default EditDocument = () => {
+	const router = useRouter();
 
-  const [docxContent, setDocxContent] = useState("");
-  const quillRef = useRef();
+	const [docxContent, setDocxContent] = useState("");
+	const quillRef = useRef();
 
-  // useEffect(() => {
-  // 	console.log(quillRef.current);
-  // }, [quillRef]);
+	// useEffect(() => {
+	// 	console.log(quillRef.current);
+	// }, [quillRef]);
 
-  const docName = router.query.data;
-  const uid = auth.uid;
-  useEffect(() => {
-    getDownloadURL(docName)
-      .then(async (url) => {
-        // Fetch the content or convert the .docx to a readable format (e.g., HTML)
-        const response = await fetch(url);
-        const docxBuffer = await response.arrayBuffer();
-        const result = await mammoth.convertToHtml({ arrayBuffer: docxBuffer });
+	const docName = router.query.data;
+	const uid = auth.uid;
+	useEffect(() => {
+		getDownloadURL(docName)
+			.then(async (url) => {
+				// Fetch the content or convert the .docx to a readable format (e.g., HTML)
+				const response = await fetch(url);
+				const docxBuffer = await response.arrayBuffer();
+				const result = await mammoth.convertToHtml({ arrayBuffer: docxBuffer });
 
-        setDocxContent(result.value);
-      })
-      .catch((error) => {
-        console.error("Error fetching the .docx file:", error);
-      });
-  }, []);
+				setDocxContent(result.value);
+			})
+			.catch((error) => {
+				console.error("Error fetching the .docx file:", error);
+			});
+	}, []);
 
-  // const handleSave = async () => {
-  //   try {
-  //     // const docxContent = await htmlToDocx(editorContent);
-  //     // const blob = new Blob([docxContent], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+	const viewer = useRef(null);
 
-  //     console.log(docxContent);
+	useEffect(() => {
+		import('@pdftron/webviewer').then(() => {
+			WebViewer(
+				{
+					path: '/webviewer/lib',
+					initialDoc: '../public/demo-annotated.pdf',
+					licenseKey: 'demo:1703695491128:7c802e3e03000000000c606c4d96767204dd2d1ace102c39222b9bda27'  // sign up to get a free trial key at https://dev.apryse.com
+				},
+				viewer.current
+			).then((instance) => {
+				const { docViewer } = instance;
+				// you can now call WebViewer APIs here...
+			});
+		});
+	}, []);
 
-  //     // const fileBuffer = await HTMLtoDOCX(docxContent, null, {
-  //     //   table: { row: { cantSplit: true } },
-  //     //   footer: true,
-  //     //   pageNumber: true,
-  //     // });
+	// const handleSave = async () => {
+	// 	try {
+	// 		const docxContent = await htmlToDocx(editorContent);
+	// 		const blob = new Blob([docxContent], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
 
-  //     const { value } = await mammoth.extractRawText({ arrayBuffer: Buffer.from(docxContent) });
-    
-  //     console.log("this is value");
+	// 		console.log(docxContent);
 
-  //     console.log(value);
+	// 		const fileBuffer = await HTMLtoDOCX(docxContent, null, {
+	// 			table: { row: { cantSplit: true } },
+	// 			footer: true,
+	// 			pageNumber: true,
+	// 		});
 
-  //     // const storageRef = ref(storage, 'path_to_your_file.docx');
-  //     // await uploadBytes(storageRef, blob);
-  //   } catch (error) {
-  //     console.error("Error converting or saving the document:", error);
-  //   }
-  // };
+	// 		const { value } = await mammoth.extractRawText({ arrayBuffer: Buffer.from(docxContent) });
 
-  return (
-    <div>
-      <Head>
-        <title>Document Edit</title>
-      </Head>
+	// 		console.log("this is value");
 
-      <main className={styles.mainBody} style={{ padding: "20px" }}>
-        <Button onClick={handleSave}>Save here</Button>
-        <ReactQuill
-          ref={quillRef}
-          theme="snow"
-          value={docxContent}
-          onChange={setDocxContent}
-        />
-      </main>
-      <AppFooter />
-    </div>
-  );
+	// 		console.log(value);
+
+	// 		// const storageRef = ref(storage, 'path_to_your_file.docx');
+	// 		// await uploadBytes(storageRef, blob);
+	// 	} catch (error) {
+	// 		console.error("Error converting or saving the document:", error);
+	// 	}
+	// };
+
+	const handleSave = async () => {
+		const fileBuffer = await HTMLtoDOCX(htmlString, null, {
+			table: { row: { cantSplit: true } },
+			footer: true,
+			pageNumber: true,
+		});
+
+		fs.writeFile(filePath, fileBuffer, (error) => {
+			if (error) {
+				console.log('Docx file creation failed');
+				return;
+			}
+			console.log('Docx file created successfully');
+		});
+	}
+
+	return (
+		<div>
+			<Head>
+				<title>Document Edit</title>
+			</Head>
+
+			<main className={styles.mainBody} style={{ padding: "20px" }}>
+				<div className='MyComponent'>
+					<div className='webviewer' ref={viewer} style={{ height: '100vh' }}></div>
+				</div>
+				<Button onClick={handleSave}>Save here</Button>
+				<ReactQuill
+					ref={quillRef}
+					theme="snow"
+					value={docxContent}
+					onChange={setDocxContent}
+				/>
+			</main>
+			<AppFooter />
+		</div>
+	);
 };
 
-export default EditDocument;
+// export default EditDocument;
 
